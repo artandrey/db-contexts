22333f8d43e6396ecb95f71f4c27a020384cab22
Andrii Artemenko <57059299+artandrey@users.noreply.github.com>
2024-11-11 23:48:52 +0200

    Revert "Feat/setup drizzle orm"

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,7 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    testTimeout: 15000,
-    hookTimeout: 25000,
-  },
-  resolve: {
-    alias: {
-      '~core': resolve(__dirname, './src/core'),
-      '~modules': resolve(__dirname, './src/modules'),
-    },
   },
 });
